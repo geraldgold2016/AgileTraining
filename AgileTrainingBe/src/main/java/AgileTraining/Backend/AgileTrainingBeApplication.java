b27a4acb5c52ce3c092b668d52cbaafd4cefb583
gerald guinto <geraldgold2016@gmail.com>
2024-07-11 17:16:17 +0200

    Start of Be
    start of be

---
+++ AgileTrainingBe/src/main/java/AgileTraining/Backend/AgileTrainingBeApplication.java
@@ -0,0 +1,13 @@
+package AgileTraining.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileTrainingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileTrainingBeApplication.class, args);
+	}
+
+}

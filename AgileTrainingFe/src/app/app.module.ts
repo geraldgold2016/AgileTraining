f7b98682a50dbba5fde7bdb86dbd7960addb921f
simranjit-Sim <153837098+simranjit-Sim@users.noreply.github.com>
2024-07-12 12:12:02 +0200

    creazione componente registrazione

--- AgileTrainingFe/src/app/app.module.ts
+++ AgileTrainingFe/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrazioneComponent } from './registrazione/registrazione.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrazioneComponent
   ],
   imports: [
     BrowserModule,

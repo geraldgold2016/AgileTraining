f7f3f48e6911f9f9d2bfc00b7054b46d4904057d
LorenzSeven <lorenzoquatela@gmail.com>
2024-07-15 17:58:13 +0200

    Pagina Home aggiornata

--- AgileTrainingFe/src/app/app.routes.ts
+++ AgileTrainingFe/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfiloUtenteComponent } from './components/profilo-utente/profilo-utente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full' },
+    {path:"home", component: HomepageComponent},
+    {path:"login", component: LoginComponent},
+    {path:"profiloUtente", component: ProfiloUtenteComponent}
+];

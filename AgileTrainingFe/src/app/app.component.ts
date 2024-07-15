bd5febafc285294c725d04740f258926c6ff84a9
simranjit-Sim <153837098+simranjit-Sim@users.noreply.github.com>
2024-07-15 15:39:34 +0200

    rotte aggiunte

--- AgileTrainingFe/src/app/app.component.ts
+++ AgileTrainingFe/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

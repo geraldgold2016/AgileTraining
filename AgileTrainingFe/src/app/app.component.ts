0d61a2e87eb81fd2187eb5470a2fcc01478c5ed5
simranjit-Sim <153837098+simranjit-Sim@users.noreply.github.com>
2024-07-17 09:37:59 +0200

    aggiornato tutti i componenti avvisi ,cambia-password,cancella Account

--- AgileTrainingFe/src/app/app.component.ts
+++ AgileTrainingFe/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,LoginComponent,HeaderComponent,FooterComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- AgileTrainingFe/src/app/components/login/login.component.ts
+++ AgileTrainingFe/src/app/components/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -11,7 +10,19 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showPassword: boolean = false;
+
   constructor(library: FaIconLibrary) {
     library.addIconPacks(fas);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+    const passwordInput = document.querySelector('.passInp') as HTMLInputElement;
+    if (this.showPassword) {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
   }
 }

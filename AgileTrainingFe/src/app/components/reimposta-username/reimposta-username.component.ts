8364417bfcb131ce4df09f480ea4d077e6f98ad6
LorenzSeven <lorenzoquatela@gmail.com>
2024-07-22 11:33:45 +0200

    modificati angular.json e reimposta-username

--- AgileTrainingFe/src/app/components/reimposta-username/reimposta-username.component.ts
+++ AgileTrainingFe/src/app/components/reimposta-username/reimposta-username.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-reimposta-username',

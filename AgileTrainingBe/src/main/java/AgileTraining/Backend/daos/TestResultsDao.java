317221e5090fb963ae4c7b8d09959e8e6e148c98
giulia <s.giulia97@gmail.com>
2024-07-23 14:44:26 +0200

    fixed minor issue

--- AgileTrainingBe/src/main/java/AgileTraining/Backend/daos/TestResultsDao.java
+++ AgileTrainingBe/src/main/java/AgileTraining/Backend/daos/TestResultsDao.java
@@ -1,17 +1,19 @@
 package AgileTraining.Backend.daos;
 
 
- org.springframework.data.jpa.repository.JpaRepository;
+
+import AgileTraining.Backend.entities.TestResult;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 
-public interface TestResultsDao extends JpaRepository<TestResults, Integer> {
+public interface TestResultsDao extends JpaRepository<TestResult, Integer> {
 
 
     @Query(value= "SELECT * FROM test_results tr WHERE tr.user_id = :userId AND tr.test_id = :testId",
     nativeQuery = true)
-    TestResults getTestResultsByUserIdAndTestId(@Param("userId")Integer userId,@Param("testId") Integer testId);
+    TestResult getTestResultsByUserIdAndTestId(@Param("userId")Integer userId,@Param("testId") Integer testId);
 
 
 }

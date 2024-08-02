6df756e90027e3f86f0f2c2bbab64da17a7c12cc
LorenzSeven <lorenzoquatela@gmail.com>
2024-08-02 12:36:36 +0200

    app.config

--- AgileTrainingFe/src/app/app.config.ts
+++ AgileTrainingFe/src/app/app.config.ts
@@ -1,8 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),]
